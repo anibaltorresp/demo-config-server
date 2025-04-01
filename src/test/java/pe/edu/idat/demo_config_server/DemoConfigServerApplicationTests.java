669c5949e9f550546ee4e62d3696ec8d56d30079
anibaltorresp <anibal.torresp@gmail.com>
2025-03-31 20:08:14 -0500

    iniciando proyecto demo config server

---
+++ src/test/java/pe/edu/idat/demo_config_server/DemoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
